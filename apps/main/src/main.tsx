fade85d5006b3c51c1a5a1a76c22cfc6baa036b9
woohm402 <woohm404@gmail.com>
2025-05-01 16:53:38 +0900

    리팩

--- apps/main/src/main.tsx
+++ apps/main/src/main.tsx
@@ -1,10 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { App } from "./App.tsx";
-import "./tailwind.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { App } from './App.tsx';
+import './tailwind.css';
 
-const root = document.getElementById("root");
-if (!root) throw new Error("no root");
+const root = document.getElementById('root');
+if (!root) throw new Error('no root');
 
 ReactDOM.createRoot(root).render(
   <React.StrictMode>
